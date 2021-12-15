1bb54399e858f41a65940bca459cbe777ae4b487
Kevin Do <kevind0501@gmail.com>
2021-12-15 09:40:52 -0800

    working on controls

--- src/components/Characters/Controls.js
+++ src/components/Characters/Controls.js
@@ -1,9 +1,36 @@
 import React from 'react';
 
-export default function Controls({ query, setQuery, race, setRace }) {
+export default function Controls({
+  query,
+  setQuery,
+  races,
+  setRace,
+  selectedRace,
+  setSelectedRace,
+}) {
+  const handleClick = (e) => {
+    setRace(e.target.value);
+  };
   return (
     <div>
-      <input type="text" placeholder="Search Name" label="Search Name" value={query}></input>
+      <input
+        type="text"
+        placeholder="Search Name"
+        label="Search Name"
+        value={query}
+        onChange={(e) => {
+          setQuery(e.target.value);
+        }}
+      ></input>
+      <select value={selectedRace} onChange={(e) => setSelectedRace(e.target.value)}>
+        <option value="All">All</option>
+        {races.map((race) => (
+          <option key={race} value={race}>
+            {race}
+          </option>
+        ))}
+      </select>
+      <button onClick={handleClick}>Search</button>
     </div>
   );
 }
--- src/views/Characters/Characters.js
+++ src/views/Characters/Characters.js
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import CharactersList from '../../../src/components/Characters/CharactersList';
+import Controls from '../../components/Characters/Controls';
 import { fetchCharacters } from '../../services/characters';
 
 export default function Characters() {
   const [characters, setCharacters] = useState([]);
   const [query, setQuery] = useState('');
+  const [races, setRaces] = useState('All');
 
   useEffect(() => {
     const getCharacters = async () => {
@@ -17,12 +19,14 @@ export default function Characters() {
   return (
     <div>
       <h1>Characters</h1>
-      <Controls query={query} setQuery={setQuery} />
+      <Controls query={query} setQuery={setQuery} races={races} setRaces={setRaces} />
       <CharactersList
         characters={characters}
         setCharacters={setCharacters}
         query={query}
         setQuery={setQuery}
+        races={races}
+        setRaces={setRaces}
       />
     </div>
   );
